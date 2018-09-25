4aff0ba9d3853766b8e772ff0b8e177e1f7eeb6a
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 21:46:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,22 +1,11 @@
-require 'pry'
+
 def oxford_comma(array)
-  size = array.size
-  if array.size > 2
-    x = array * " , "
-    y = x.split(" ")
-
-    y.insert(y.size-1, "and")
-    y.collect {|fruit| fruit + " "} 
-    
-
-y
-
-
-
-
-  elsif array.size == 2
-    return array.join(" and ")
-  else
-    return array.join
-  end
+  case array.length
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end
 end
