6a7baa4b069cfbb087789fbb668ffcd4a0d331e9
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 21:18:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,19 @@
-require 'pry'
 def oxford_comma(array)
+  size = array.size
+  if array.size > 2
+    array.insert(size-1, "and")
 
-  l = array.length
+    word = array.join(", ")
+    array_2 = word.split(" ")
 
-  if l == 1
-    array.to_s
-  elsif l == 2
-    puts array.join(" and ")
+    final = array_2.join(" ")
+    return final
+
+
+
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array.join
   end
-
-
 end
