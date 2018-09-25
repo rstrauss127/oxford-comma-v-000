f3c1f41c0e2a60f553be3d9d08a56ec4af5a738e
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 21:07:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,4 @@
+require 'pry'
 def oxford_comma(array)
-  if array.size > 2
-    word = array.join(", ")
-    array_2 = word.split(" ")
-    size = array_2.size
-    array_2.insert(size-1, "and")
-    final = array_2.join(" ")
-    return final
-  elsif array.size == 2 
-    return array.join(" and ")
-  else
-    return array.join
-  end
+binding.pry
 end
